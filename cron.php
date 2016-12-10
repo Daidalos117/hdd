73170f6e0ed8878b5df13e0fb64f7f7f893dd033
Roman Rajchert <rajchert.roman@gmail.com>
2016-12-10 21:33:04 +0100

    Page with movies

---
+++ cron.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * User: romanrajchert
+ * Date: 10.12.16
+ * Time: 17:05
+ * Project: hdd
+ */
+
+$dir = "/Volumes/Seagate Backup Plus Drive/";
+if(!dir($dir)){
+    die();
+}
+require_once "Databaze.php";
+Databaze::pripoj('localhost', 'root', 'root', 'hdd');
+
+
+$query = Databaze::dotaz("SELECT * FROM directories");
+$moviesDirectories = $query->fetchAll(PDO::FETCH_ASSOC);
+
+$filesExceptions = [".", ".."];
+foreach ($moviesDirectories as $key => $directory){
+    $newDir = $dir.$directory["directory"];
+    $scanDir = scandir($newDir);
+
+    foreach ($scanDir as $file){
+        if(in_array($file,$filesExceptions)) continue;
+
+        $newFile = $newDir."/".$file;
+        $fileSize = filesize($newFile);
+
+        $time = filemtime($newFile);
+
+        $query = Databaze::dotaz("SELECT * FROM files WHERE file = ? AND directory_id = ?",[$file,$directory["id"]]);
+        $exists = $query->fetch();
+
+        $year = null;
+        if (preg_match('/\b\d{4}\b/', $file, $matches)) {
+            $year = $matches[0];
+        }
+        if($exists){
+            Databaze::dotaz("UPDATE `files` set last_seen = NOW(), file_edited = FROM_UNIXTIME(?) WHERE file = ? AND directory_id = ?",[$time,$file,$directory["id"]]);
+        }else{
+            $qu = Databaze::dotaz("INSERT INTO `files`( `file`, `last_seen`, `directory_id`, `year`, `size_bytes`,  `file_edited`) VALUES (?,NOW(),?,?,?,FROM_UNIXTIME(?))",
+                [$file,$directory["id"], $year, $fileSize, $time]);
+
+        }
+
+    }
+
+
+}
--- index.php
+++ index.php
@@ -1,33 +1,80 @@
-<?php
-/**
- * User: romanrajchert
- * Date: 10.12.16
- * Time: 17:05
- * Project: hdd
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
 
-$dir = "/Volumes/Seagate Backup Plus Drive/";
-if(!dir($dir)){
-    die();
-}
-require_once "Databaze.php";
-Databaze::pripoj('localhost', 'root', 'root', 'hdd');
+    <link rel="stylesheet" type="text/css" href="src/datatables.min.css"/>
+    <link rel="stylesheet" href="src/bootstrap.min.css">
+    <style>
+        h1{
+            font-size: 10rem;
+            text-align: center;
+            margin: 1rem;
+        }
+    </style>
 
-$query = Databaze::dotaz("SELECT * FROM directories");
-$moviesDirectories = $query->fetchAll(PDO::FETCH_ASSOC);
+</head>
+
+<body>
+<div class="container">
+<h1>Movies</h1>
+<table id="table" class="table  table-striped" cellspacing="0" width="100%">
+    <thead>
+    <tr>
+        <th>File</th>
+        <th>Year</th>
+        <th>Created</th>
+        <th>Last seen</th>
+        <th>Directory</th>
+        <th>Size</th>
+        <th>File edited</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php
+    function human_filesize($bytes, $decimals = 2) {
+        $sz = 'BKMGTP';
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+    }
+
+    require_once "Databaze.php";
+    Databaze::pripoj('localhost', 'root', 'root', 'hdd');
+    $query = Databaze::dotaz("SELECT * FROM files left join directories on files.directory_id=directories.id");
+    $files = $query->fetchAll();
+    foreach ($files as $file){
+        echo "<tr>";
+        echo "<td>".$file["file"]."</td>";
+        echo "<td>".$file["year"]."</td>";
+        echo "<td>".$file["created"]."</td>";
+        echo "<td>".$file["last_seen"]."</td>";
+        echo "<td>".$file["directory"]."</td>";
+        echo "<td>".human_filesize($file["size_bytes"])."</td>";
+        echo "<td>".$file["file_edited"]."</td>";
 
 
-foreach ($moviesDirectories as $key => $directory){
-    $scanDir = scandir($dir.$directory["directory"]);
-
-    foreach ($scanDir as $file){
-
-        $query = Databaze::dotaz("SELECT * FROM files WHERE file = ? AND directory = ?",[$file,$directory["id"]]);
-        $exists = ;
-
-
-
+        echo "</tr>";
     }
 
 
-}
+    ?>
+
+
+    </tbody>
+
+
+</div>
+<script type="text/javascript" src="src/jQuery-2.2.4/jquery-2.2.4.min.js"></script>
+<script type="text/javascript" src="src/Bootstrap-3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="src/datatables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#table').DataTable({
+                "pageLength": 100
+            });
+        } );
+    </script>
+</body>
+
+</html>
