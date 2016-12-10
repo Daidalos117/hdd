1b0fa1b18a132b00a57e767963668f9b3bd2cdf3
Roman Rajchert <rajchert.roman@gmail.com>
2016-12-10 22:23:26 +0100

    Time formats and other

--- index.php
+++ index.php
@@ -12,13 +12,38 @@
             text-align: center;
             margin: 1rem;
         }
+        .db-update{
+            font-size: 20px;
+            margin-bottom: 10px;
+            display: inline-block;
+        }
     </style>
 
 </head>
 
 <body>
+<?php
+function human_filesize($bytes, $decimals = 2) {
+    $sz = 'BKMGTP';
+    $factor = floor((strlen($bytes) - 1) / 3);
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
+}
+
+require_once "Databaze.php";
+Databaze::pripoj('localhost', 'root', 'root', 'hdd');
+$query = Databaze::dotaz("SELECT max(last_seen) as 'last' FROM files");
+$last_seen = $query->fetch();
+date_default_timezone_set("Europe/Prague");
+$last_seen = new DateTime($last_seen["last"]);
+$now = new DateTime();
+$diff = $last_seen->diff($now)->format("%d");
+$label_color = ($diff > 3) ? "label-danger" : "label-info";
+?>
 <div class="container">
 <h1>Movies</h1>
+
+    <h4>Last DB update </h4>
+    <span class="label <?=$label_color?> db-update"><?php echo $last_seen->format("d.m.y H:m") ?></span>
 <table id="table" class="table  table-striped" cellspacing="0" width="100%">
     <thead>
     <tr>
@@ -33,34 +58,24 @@
     </thead>
     <tbody>
     <?php
-    function human_filesize($bytes, $decimals = 2) {
-        $sz = 'BKMGTP';
-        $factor = floor((strlen($bytes) - 1) / 3);
-        return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
-    }
-
-    require_once "Databaze.php";
-    Databaze::pripoj('localhost', 'root', 'root', 'hdd');
     $query = Databaze::dotaz("SELECT * FROM files left join directories on files.directory_id=directories.id");
     $files = $query->fetchAll();
     foreach ($files as $file){
         echo "<tr>";
         echo "<td>".$file["file"]."</td>";
         echo "<td>".$file["year"]."</td>";
-        echo "<td>".$file["created"]."</td>";
-        echo "<td>".$file["last_seen"]."</td>";
+        $created = new DateTime($file["created"]);
+        echo "<td><span data-toggle='tooltip' title='".$created->format("H:m:s")."'> ".$created->format("d.m.y")."</span></td>";
+        $last = new DateTime($file["last_seen"]);
+        echo "<td><span data-toggle='tooltip' title='".$last->format("H:m:s")."'> ".$last->format("d.m.y")."</span></td>";
         echo "<td>".$file["directory"]."</td>";
         echo "<td>".human_filesize($file["size_bytes"])."</td>";
-        echo "<td>".$file["file_edited"]."</td>";
-
-
+        $edit = new DateTime($file["file_edited"]);
+        echo "<td><span data-toggle='tooltip' title='".$edit->format("H:m:s")."'> ".$edit->format("d.m.y")."</span></td>";
         echo "</tr>";
     }
 
-
     ?>
-
-
     </tbody>
 
 
@@ -73,6 +88,9 @@
             $('#table').DataTable({
                 "pageLength": 100
             });
+
+            $('[data-toggle="tooltip"]').tooltip();
+
         } );
     </script>
 </body>
